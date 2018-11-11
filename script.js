75d7aae9859e849255f9dc3ba8e44d5c1d9914cc
Chariesa Roozendaal <chariesa@hotmail.com>
2018-11-11 14:49:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,674 @@
+// console.log('hallo wereld');
+let canvas = document.getElementById('canvas');
+let context = canvas.getContext('2d');
+
+//negende afdak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(332,376);
+context.lineTo(333,279);
+context.lineTo(381,252);
+context.lineTo(430,279);
+context.lineTo(431,376);
+context.closePath();
+context.stroke();
+context.fill();
+
+//Eerste pad
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#404040";
+context.moveTo(152,146);
+context.lineTo(397,37);
+context.lineTo(640,146);
+context.closePath();
+context.stroke();
+context.fill();
+
+//Tweede pad schoorsteen
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(232,85);
+context.lineTo(251,85);
+context.lineTo(251,100);
+context.lineTo(232,109);
+context.closePath();
+context.stroke();
+context.fill();
+
+//Derde pad linksdriehoek
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#404040";
+context.moveTo(163,148);
+context.lineTo(206,148);
+context.lineTo(206,201);
+context.closePath();
+context.stroke();
+context.fill();
+
+//Derde pad rechts driehoek
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#404040";
+context.moveTo(602,149);
+context.lineTo(644,149);
+context.lineTo(602,184);
+context.closePath();
+context.stroke();
+context.fill();
+
+//Vierde pad middendriehoek
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(191,140);
+context.lineTo(399,44);
+context.lineTo(604,140);
+context.closePath();
+context.stroke();
+context.fill();
+
+//Vijfde pad middendriehoek
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#404040";
+context.moveTo(399,66);
+context.lineTo(480,98);
+context.lineTo(324,98);
+context.closePath();
+context.stroke();
+context.fill();
+
+//Zesde pad 1raampjes
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#ffffff";
+context.moveTo(342,101);
+context.lineTo(360,101);
+context.lineTo(360,123);
+context.lineTo(342,123);
+context.closePath();
+context.stroke();
+context.fill();
+
+//Zesde pad 2raampjes
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#ffffff";
+context.moveTo(363,101);
+context.lineTo(374,101);
+context.lineTo(374,112);
+context.lineTo(363,112);
+context.closePath();
+context.stroke();
+context.fill();
+
+//Zesde pad 3raampjes
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#ffffff";
+context.moveTo(363,112);
+context.lineTo(374,112);
+context.lineTo(374,123);
+context.lineTo(363,123);
+context.closePath();
+context.stroke();
+context.fill();
+
+//Zesde pad 4raampjes
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#ffffff";
+context.moveTo(376,101);
+context.lineTo(389,101);
+context.lineTo(389,110);
+context.lineTo(376,110);
+context.closePath();
+context.stroke();
+context.fill();
+
+//Zesde pad 5 raampjes
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#ffffff";
+context.moveTo(376,112);
+context.lineTo(389,112);
+context.lineTo(389,123);
+context.lineTo(376,123);
+context.closePath();
+context.stroke();
+context.fill();
+
+//negende afdak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(313,379);
+context.lineTo(313,280);
+context.lineTo(330,280);
+context.lineTo(330,379);
+context.closePath();
+context.stroke();
+context.fill();
+
+
+//Zesde pad 6 raampjes
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#ffffff";
+context.moveTo(389,101);
+context.lineTo(417,101);
+context.lineTo(417,123);
+context.lineTo(389,123);
+context.closePath();
+context.stroke();
+context.fill();
+
+//Zesde pad 7 raampjes
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#ffffff";
+context.moveTo(417,101);
+context.lineTo(431,101);
+context.lineTo(431,112);
+context.lineTo(417,112);
+context.closePath();
+context.stroke();
+context.fill();
+
+//Zesde pad 8 raampjes
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#ffffff";
+context.moveTo(417,112);
+context.lineTo(431,112);
+context.lineTo(431,123);
+context.lineTo(417,123);
+context.closePath();
+context.stroke();
+context.fill();
+
+//Zesde pad 9 raampjes
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#ffffff";
+context.moveTo(431,101);
+context.lineTo(445,101);
+context.lineTo(445,110);
+context.lineTo(431,110);
+context.closePath();
+context.stroke();
+context.fill();
+
+//Zesde pad 10 raampjes
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#ffffff";
+context.moveTo(431,112);
+context.lineTo(445,112);
+context.lineTo(445,123);
+context.lineTo(431,123);
+context.closePath();
+context.stroke();
+context.fill();
+
+//Zesde pad 11 raampjes
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#ffffff";
+context.moveTo(445,101);
+context.lineTo(463,101);
+context.lineTo(463,123);
+context.lineTo(445,123);
+context.closePath();
+context.stroke();
+context.fill();
+
+//Zevende pad 1 bovenkant
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(208,147);
+context.lineTo(437,147);
+context.lineTo(437,160);
+context.lineTo(208,160);
+context.closePath();
+context.stroke();
+context.fill();
+
+// //negende afdak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(483,274);
+context.lineTo(483,363);
+context.lineTo(449,362);
+context.lineTo(449,282);
+context.lineTo(463,281);
+context.lineTo(460,274);
+context.closePath();
+context.stroke();
+context.fill();
+
+//Zevende pad 2 bovenkant
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(437,147);
+context.lineTo(484,147);
+context.lineTo(484,156);
+context.lineTo(437,161);
+context.closePath();
+context.stroke();
+context.fill();
+
+//Zevende pad 3 bovenkant
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(484,147);
+context.lineTo(549,147);
+context.lineTo(549,156);
+context.lineTo(484,156);
+context.closePath();
+context.stroke();
+context.fill();
+
+//Zevende pad 4 bovenkant
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(552,147);
+context.lineTo(599,147);
+context.lineTo(599,162);
+context.lineTo(552,156);
+context.closePath();
+context.stroke();
+context.fill();
+
+//achtste groot vlak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(208,163);
+context.lineTo(434,163);
+context.lineTo(434,259);
+context.lineTo(208,278);
+context.closePath();
+context.stroke();
+context.fill();
+
+//achtste groot vlak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(437,163);
+context.lineTo(482,159);
+context.lineTo(482,242);
+context.lineTo(437,245);
+context.closePath();
+context.stroke();
+context.fill();
+
+//achtste groot vlak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(485,158);
+context.lineTo(549,158);
+context.lineTo(549,242);
+context.lineTo(485,242);
+context.closePath();
+context.stroke();
+context.fill();
+
+//achtste groot vlak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(552,158);
+context.lineTo(599,163);
+context.lineTo(599,246);
+context.lineTo(552,242);
+context.closePath();
+context.stroke();
+context.fill();
+
+//negende afdak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(167,289);
+context.lineTo(302,247);
+context.lineTo(351,247);
+context.lineTo(297,274);
+context.lineTo(297,280);
+context.lineTo(313,280);
+context.lineTo(313,289);
+context.closePath();
+context.stroke();
+context.fill();
+
+//negende afdak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(382,231);
+context.lineTo(463,276);
+context.lineTo(463,279);
+context.lineTo(430,279);
+context.lineTo(381,253);
+context.lineTo(332,279);
+context.lineTo(297,279);
+context.lineTo(298,275);
+context.closePath();
+context.stroke();
+context.fill();
+
+//negende afdak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(437,259);
+context.lineTo(437,245);
+context.lineTo(482,243);
+context.lineTo(482,273);
+context.lineTo(464,273);
+context.lineTo(464,273);
+context.closePath();
+context.stroke();
+context.fill();
+
+//negende afdak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(485,243);
+context.lineTo(549,243);
+context.lineTo(549,273);
+context.lineTo(485,273);
+context.closePath();
+context.stroke();
+context.fill();
+
+//negende afdak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(552,243);
+context.lineTo(599,247);
+context.lineTo(599,276);
+context.lineTo(552,273);
+context.closePath();
+context.stroke();
+context.fill();
+
+//negende afdak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(485,276);
+context.lineTo(549,276);
+context.lineTo(549,363);
+context.lineTo(485,363);
+context.closePath();
+context.stroke();
+context.fill();
+
+//negende afdak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(551,276);
+context.lineTo(599,278);
+context.lineTo(599,364);
+context.lineTo(551,363);
+context.closePath();
+context.stroke();
+context.fill();
+
+//negende afdak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(551,365);
+context.lineTo(599,365);
+context.lineTo(599,405);
+context.lineTo(551,406);
+context.closePath();
+context.stroke();
+context.fill();
+
+//negende afdak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(485,365);
+context.lineTo(548,365);
+context.lineTo(548,406);
+context.lineTo(485,406);
+context.closePath();
+context.stroke();
+context.fill();
+
+//negende afdak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(449,365);
+context.lineTo(483,365);
+context.lineTo(483,406);
+context.lineTo(449,406);
+context.closePath();
+context.stroke();
+context.fill();
+
+//negende afdak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(430,282);
+context.lineTo(447,282);
+context.lineTo(447,375);
+context.lineTo(431,375);
+context.closePath();
+context.stroke();
+context.fill();
+
+//negende afdak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(447,375);
+context.lineTo(447,380);
+context.lineTo(431,380);
+context.lineTo(431,375);
+context.closePath();
+context.stroke();
+context.fill();
+
+//negende afdak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(447,383);
+context.lineTo(447,414);
+context.lineTo(431,414);
+context.lineTo(431,383);
+context.closePath();
+context.stroke();
+context.fill();
+
+//negende afdak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(472,447);
+context.lineTo(228,446);
+context.lineTo(313,418);
+context.lineTo(450,415);
+context.closePath();
+context.stroke();
+context.fill();
+
+//negende afdak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(429,413);
+context.lineTo(332,415);
+context.lineTo(332,409);
+context.lineTo(429,408);
+context.closePath();
+context.stroke();
+context.fill();
+
+//negende afdak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(429,405);
+context.lineTo(332,407);
+context.lineTo(332,402);
+context.lineTo(429,398);
+context.closePath();
+context.stroke();
+context.fill();
+
+//negende afdak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(429,399);
+context.lineTo(332,400);
+context.lineTo(332,394);
+context.lineTo(429,393);
+context.closePath();
+context.stroke();
+context.fill();
+
+//negende afdak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(332,392);
+context.lineTo(332,385);
+context.lineTo(429,385);
+context.lineTo(429,391);
+context.closePath();
+context.stroke();
+context.fill();
+
+//negende afdak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(332,377);
+context.lineTo(428,377);
+context.lineTo(428,383);
+context.lineTo(332,383);
+context.closePath();
+context.stroke();
+context.fill();
+
+//negende afdak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(351,370);
+context.lineTo(414,370);
+context.lineTo(414,374);
+context.lineTo(351,374);
+context.closePath();
+context.stroke();
+context.fill();
+
+//negende afdak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(315,416);
+context.lineTo(315,386);
+context.lineTo(329,386);
+context.lineTo(329,416);
+context.closePath();
+context.stroke();
+context.fill();
+
+//negende afdak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(331,383);
+context.lineTo(312,383);
+context.lineTo(312,378);
+context.lineTo(331,378);
+context.closePath();
+context.stroke();
+context.fill();
+
+// //negende afdak
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "#000000";
+context.fillStyle = "#80817d";
+context.moveTo(312,406);
+context.lineTo(195,406);
+context.lineTo(195,292);
+context.lineTo(312,292);
+context.closePath();
+context.stroke();
+context.fill();
